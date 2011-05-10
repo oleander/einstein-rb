2e91c51beeac8f9e69afceb1b3637454d0b20cfc
Linus Oleander <linus@oleander.nu>
2011-05-10 17:15:15 +0200

    Adding spec

---
+++ spec/einstein_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe Einstein do
+  it "should be true" do
+    true.should be_true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+require "rspec"
+require "webmock/rspec"
+require "einstein"
+require "vcr"
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.config do |c|
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.stub_with :webmock
+  c.default_cassette_options = {
+    record: :all
+  }
+  c.allow_http_connections_when_no_cassette = false
+end
